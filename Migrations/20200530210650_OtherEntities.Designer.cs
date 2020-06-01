1774885ee295679264041b4b7c5ff2c821b1e0a7
Gabriel <gabriel_3404@hotmail.com>
2020-06-01 15:44:46 -0300

    Seeding service

--- Migrations/20200530210650_OtherEntities.Designer.cs
+++ Migrations/20200530210650_OtherEntities.Designer.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Migrations
 {
